1914b753ecf7c8119b9d0ba8ec42e0cc1ff7ef1b
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-11-27 15:58:50 +0100

    Complete this source code in order to print the integer stored in the variable number

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 number = 98
-name = 'Battery street'
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-result = f"{number} Battery street\n"
-print(result)
+print(f"{number:d} Battery street\n")
