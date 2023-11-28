0e7224c9e4e119e330da045babe8f825c282f2a7
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-11-28 04:08:04 +0100

    9-easter_egg.py

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 tittle = "The Zen of Python, by TimPeters"
-"""
-Beautiful is better than ugly.
+body_of_script = """Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 """
