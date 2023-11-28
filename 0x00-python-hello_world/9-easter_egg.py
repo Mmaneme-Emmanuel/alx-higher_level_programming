d25e9e4063223d9e399ad8baaa3fe8231f213bbe
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-11-28 10:40:39 +0100

    9-easter_egg.py

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 tittle = "The Zen of Python, by Tim Peters"
-body_of_script = """Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-"""
 print(tittle)
