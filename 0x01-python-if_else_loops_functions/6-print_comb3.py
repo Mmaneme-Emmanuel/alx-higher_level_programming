028202d85ca3a26e9dd6ff39c1cf534b68eece2e
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-11-29 23:59:57 +0100

    ./6-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(99):
+    for k in range(i + 1, 10):
+        if i * 10 + k < 89:
+                print("{:d}{:d}".format(i,k),end= ",")
+                print("{:d}".format(89))
+
