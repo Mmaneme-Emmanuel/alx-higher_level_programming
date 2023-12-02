8f38b8b6f26e1e2c01edc974851f7aeee595a179
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-12-02 23:08:37 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1
 
     if num_args == 0:
-        print("Number of argument(s): 0 arguments.")
+        print("0 arguments.")
     elif num_args == 1:
-        print(f"Number of argument(s): 1 argument: \n1: {sys.argv[1]}")
+        print("1 argument:")
     else:
-        print(f"Number of argument(s): {num_args} arguments: ")
-        for i in range(1, num_args + 1):
-            print(f"{i}: {sys.argv[i]}")
-        print()
+        print("{} arguments:".format(num_args))
+
+    for i in range(1, num_args + 1):
+        print(f"{i}: {sys.argv[i]}")
