7a7ef3bcc4b43fe9bd0eef7beb540fed3ee5385d
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2023-12-05 03:13:19 +0100

    Write a function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,2 +1,6 @@
 def print_list_integer(my_list=[]):
 def element_at(my_list, idx):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+ef print_reversed_list_integer(my_list=[]):
+def print_matrix_integer(matrix=[[]]):	
