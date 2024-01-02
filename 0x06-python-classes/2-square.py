686b0bc2039fb3788d360edb0d1ce60c81145bd6
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-01-02 10:00:53 +0100

    ./2-main.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Class of square"""
+"""Define a class square."""
 
 
 class Square:
-    """Defining a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
