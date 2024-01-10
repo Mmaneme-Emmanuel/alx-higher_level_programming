af7ca7bd2f7580d22633f3db6d1e1552538e1b86
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-01-10 01:36:46 +0100

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,18 +3,19 @@
 
 
 def inherits_from(obj, a_class):
-    """if the object is exactly an instance of the specified class.
+    """Check if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
 
     Args:
         obj: The object to be checked.
         a_class: The class to check against.
 
     Returns:
-        True if the object is an instance of the specified class
-        False otherwise.
+        True if the object is an instance of a class that inherited
+        from the specified class, otherwise False.
     """
 
-    if issubclass(type(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
