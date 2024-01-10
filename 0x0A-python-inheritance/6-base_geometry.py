aaa3fa8d9d86f9247a30f3b69053c2c6dfc42409
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-01-10 02:02:08 +0100

    Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a class of basegeometry"""
+
+
+class BaseGeometry:
+    """Public instance method: def area(self):"""
+
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raises Exception("area() is not implemented")
