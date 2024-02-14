e650f6ebc8be5da4364e8236103c73f5c94bdaf7
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-02-14 16:12:07 +0100

    done

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
-__ prints the full description of the table first_table from the database
+-- Prints the full description of the table first_table from the database
 SHOW CREATE TABLE 'first_table';
