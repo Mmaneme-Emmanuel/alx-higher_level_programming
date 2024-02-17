f81535d45f72ebaf9c13a306a4ba794a76cc8ef9
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-02-17 13:46:56 +0100

    more of sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)
+-- Import the database dump of hbtn_0d_tvshows to your MySQL server:
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
