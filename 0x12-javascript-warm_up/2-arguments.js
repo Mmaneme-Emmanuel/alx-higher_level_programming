3be4930fab84dde6e7c983a98415b46b6e88c90e
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-03-12 14:59:26 +0100

    script that prints 3 lines:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
-const args = process.argv.length;
-
-if (args === 2) {
+if (process.argv.length === 2) {
     console.log('No argument');
 } else if (args === 3) {
     console.log('Argument found');
