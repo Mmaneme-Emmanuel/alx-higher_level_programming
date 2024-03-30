d3b5543241ec630076e2ad520cb1e3b11ad7c29b
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-03-30 21:26:35 +0100

    empty class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-        if ((w > 0)) && ((h > 0)) {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
             this.width = w;
             this.height = h;
         } else {
+            // Create an empty object if w or h is not positive
             this.width = {};
             this.height = {};
         }
