f2b7a4bd76a13801cff6bfa266ad2a185298c8aa
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-01 16:07:37 +0100

    empty class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,30 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-		if((w > 0; && (h > 0)) {
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print(){
-		row = '';
-		for(let i = 0; i < this.width; i++) {
-			for(let j = 0; k < 0 this.height; j++) {
-				row += 'X';
-			}
-			console.log(row)
-		}
-	}
-	rotate() {
-		{this.width, this.height} = {this.height, this.width}
-	}
-	double() {
-		this.width *= 2;
-		this.height *= 2;
-	}
+        constructor (w, h) {
+                if (w > 0 && h > 0) {
+                        this.width = w;
+                        this.height = h;
+                } else {
+                        this.width = 0;
+                        this.height = 0;
+                }
+        }
+        print(){
+                let row = '';
+                for(let i = 0; i < this.height; i++) {
+                        row = '';
+                        for(let j = 0; j < this.width; j++) {
+                                row += 'X';
+                        }
+                        console.log(row);
+                }
+        }
+        rotate() {
+                [this.width, this.height] = [this.height, this.width];
+        }
+        double() {
+                this.width *= 2;
+                this.height *= 2;
+        }
 }
-module.export = rectangle;
+module.exports = Rectangle;
