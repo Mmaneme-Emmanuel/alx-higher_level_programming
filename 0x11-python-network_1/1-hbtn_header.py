066302ef89fd66c9d784e8887eff101ef299261b
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 00:54:04 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,11 +4,11 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
+        print("Usage: ./script.py <URL>")
         sys.exit(1)
 
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.info().get('X-Request-Id')
+        x_request_id = response.getheader('X-Request-Id')
         print("X-Request-Id:", x_request_id)
