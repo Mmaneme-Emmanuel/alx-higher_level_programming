2c32591732403fd31cc16221a5edb3d3b80b31b5
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 11:32:27 +0100

    2-post_email.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,7 @@ https://alx-intranet.hbtn.io/status
 """
 import requests
 
+
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     data = response.text
