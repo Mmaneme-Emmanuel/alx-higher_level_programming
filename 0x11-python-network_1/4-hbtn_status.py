a93de774dd2cf7248c623afe869952304b326f54
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 11:35:23 +0100

    2-post_email.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches
-https://alx-intranet.hbtn.io/status
-"""
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alx-intranet.hbtn.io/status")
-    data = response.text
-
-    print("Body response")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
