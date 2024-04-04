39537b42f5c0f81129f34c4e5dfc936337fb7679
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 15:28:35 +0100

    8-json_api.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,10 +2,15 @@
 """Python script that takes your GitHub credentials"""
 import sys
 import requests
-from requests.auth import HTTPBasicAuth
-
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    """Set up the authentication header with the personal access token"""
+    auth = (username, password)
+
+    """Send a GET request to the GitHub API to retrieve user information"""
     response = requests.get("https://api.github.com/user", auth=auth)
+    """Print the user's ID"""
     print(response.json().get("id"))
