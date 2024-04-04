2a508bc86136cbd5bc4803c0d0e0e7749822afa6
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 12:34:26 +0100

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches https://alx-"""
-import requests
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./5-hbtn_header.py <URL>
+"""
 import sys
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
-    print(response.header.get("X-Request-Id"))
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
