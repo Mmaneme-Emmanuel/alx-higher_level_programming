b5a4488047265c796238bff7745338bf326fe12a
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 15:32:48 +0100

    8-json_api.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,10 +7,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    """Set up the authentication header with the personal access token"""
     auth = (username, password)
 
-    """Send a GET request to the GitHub API to retrieve user information"""
     response = requests.get("https://api.github.com/user", auth=auth)
-    """Print the user's ID"""
     print(response.json().get("id"))
