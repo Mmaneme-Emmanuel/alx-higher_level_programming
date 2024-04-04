7821dde618985787cf4bf836dc5eab662b1e6c88
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-04-04 01:39:46 +0100

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL"""
 import urllib.request
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-
+    request = urllib.request.Request(url)
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
+        x_request_id = dict(response.headers).get("X-Request-Id")
         print("X-Request-Id:", x_request_id)
